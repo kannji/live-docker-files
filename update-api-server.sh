aebe7c9511281e4b0bbec45975528d6c57bec34c
Jan-Luca Klees <email@janlucaklees.de>
2017-03-02 17:42:57 +0100

    Delete update-api-server.sh

--- update-api-server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker stop kannji-api-server
-docker rm kannji-api-server
-docker rmi kannji-api-server
-docker build -t kannji-api-server /home/kannji/api-server-docker
-docker run -d --name kannji-api-server -p 80:80 kannji-api-server
