07d9f650a931c27fe387acd47dcce1869f936668
Jan-Luca Klees <4klees@informatik.uni-hamburg.de>
2017-03-02 13:55:28 +0100

    changed port to 80

--- update-api-server.sh
+++ update-api-server.sh
@@ -3,4 +3,4 @@ docker stop kannji-api-server
 docker rm kannji-api-server
 docker rmi kannji-api-server
 docker build -t kannji-api-server /home/kannji/api-server-docker
-docker run -d --name kannji-api-server -p 8000:80 kannji-api-server
+docker run -d --name kannji-api-server -p 80:80 kannji-api-server
