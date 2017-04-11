7c056b6c5c2f140abb14fda34e8a5408794ac4ee
Jan-Luca Klees <4klees@informatik.uni-hamburg.de>
2017-04-11 19:26:54 +0200

    - reorganised everything. - now using docker-compose - now using nginx and let's encrypt

--- api/docker/3rd-party-base/rebuild-kannji-api-3rd-party-base-image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# go to this dir
-cd /kannji/api/docker/3rd-party-base/
-
-# stop and remove eventually running containers and remove the old image to be able to reuse the name
-sh ../remove-docker-image.sh kannji-api-server-
-# build the new docker image
-docker build --no-cache -t kannji-api-3rd-party-base-image .
--- docker/nginx/setup nginx.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -it -p 80:80 -p 443:443 ubuntu
-
-# in container
-apt-get update
-apt-get install -y nginx
-apt-get install -y letsencrypt
